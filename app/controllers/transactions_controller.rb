1e7797ad08b5bce7cae39f519bdba8b89796db02
em-gazelle <emmaf9@gmail.com>
2014-11-16 22:41:36 -0800

    Venmo payment sent manually through controller - first step of API integration. No user interaction, and no OAuth

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,37 +5,6 @@ class TransactionsController < ApplicationController
   # GET /transactions.json
   def index
     @transactions = Transaction.all
-
-    # @access_token = 'grR2SCEm2C7HdWGukWQEJyW6RpEWJYp3'
-    # puts "My Access Token to the Venmo API: #{@access_token}"
-
-
-    # MYYYY user id is: '1521929085255680697'
-  
-    # Use the class methods to get down to business quickly
-    # response = HTTParty.get('https://api.stackexchange.com/2.2/questions?site=stackoverflow')
-
-    # https://api.venmo.com/v1/payments?access_token=<access_token>
-
-    # response = HTTParty.get('https://api.venmo.com/v1/payments?access_token=' + @access_token)
-    # puts response.body, response.code, response.message, response.headers.inspect
-
-    # https://sandbox-api.venmo.com/v1/payments?limit=1&access_token=grR2SCEm2C7HdWGukWQEJyW6RpEWJYp3
-    # Returns last payment made
-
-    # https://sandbox-api.venmo.com/v1/payments/1541549070684783037?access_token=grR2SCEm2C7HdWGukWQEJyW6RpEWJYp3
-    # Returns payment [id]
-
-    # https://sandbox-api.venmo.com/v1/payments?user_id=145434160922624933&phone=15555555555&email=venmo%40venmo.com&amount=0.10&note=api%20playyya&audience=private
-
-    # POST /v1/payments?access_token=4wBq6AbfMrnE53mVMT2CvpPzynXwE7GP&user_id=145434160922624933&phone=15555555555&email=venmo%40venmo.com&amount=0.10&note=api%20playyya&audience=private
-
-    # POST request to sample Venmo API payment (fields inflexible)
-
-
-    # FROM THE COMMAND LINE...........THIS POST PAYMENT WORKED--IN REAL LIFE!!!!!!!!!!!!!!!!!!!
-    # curl https://api.venmo.com/v1/payments -d access_token=grR2SCEm2C7HdWGukWQEJyW6RpEWJYp3 -d phone=16612107788 -d amount=3 -d note="playing" -d audience="private"
-
   end
 
   # GET /transactions/1
@@ -59,21 +28,12 @@ class TransactionsController < ApplicationController
 
     @transaction.pre_tip_cost = @transaction.pre_tip_cost*100
     @transaction.post_tip = (@transaction.pre_tip_cost * (@transaction.tip_percentage+100))/100
-    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    puts "pre tip : #{@transaction.pre_tip_cost}"
-    puts "final amount"
-    puts @transaction.post_tip
-    puts "number splitting"
-    puts @transaction.number_splitting
     @transaction.individual_contribution = @transaction.post_tip / (@transaction.number_splitting + 1)
-    puts @transaction.individual_contribution
 
-
-    # THIS WORKS!!!!!!!!! HTTP POST REQUEST works IRL
-    # puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    # response = HTTParty.post("https://api.venmo.com/v1/payments?access_token=grR2SCEm2C7HdWGukWQEJyW6RpEWJYp3&phone=16612107788&amount=3&note=api%20play&audience=private")
-    # puts response.body, response.code, response.message, response.headers.inspect
-
+    # Static POST request sent via Venmo's API
+    # No OAuth - my access token used, and all parameters manually input from controller
+    response = HTTParty.post("https://api.venmo.com/v1/payments?access_token=grR2SCEm2C7HdWGukWQEJyW6RpEWJYp3&phone=16612107788&amount=3&note=api%20play&audience=private")
+    puts response.body, response.code, response.message, response.headers.inspect
 
 
     respond_to do |format|
